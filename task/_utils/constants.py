cbd7398955b683f2030a2ab5195cd474d55cbdd5
Serhii Kroshka <pm012@ukr.net>
2025-12-08 16:48:20 +0200

    first part of task is completed

--- task/_utils/constants.py
+++ task/_utils/constants.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv
 
-DIAL_URL = 'https://ai-proxy.lab.epam.com'
+load_dotenv()
+DIAL_URL = os.getenv('DIAL_API_BASE', 'https://ai-proxy.lab.epam.com')
 DIAL_CHAT_COMPLETIONS_ENDPOINT = DIAL_URL + '/openai/deployments/{model}/chat/completions'
 API_KEY = os.getenv('DIAL_API_KEY', '')
--- task/image_to_text/openai/task_openai_itt.py
+++ task/image_to_text/openai/task_openai_itt.py
@@ -1,11 +1,16 @@
 import base64
+from http import client
+import os
+import sys
 from pathlib import Path
+
+# Add workspace root to Python path
+sys.path.insert(0, str(Path(__file__).parent.parent.parent.parent))
 
 from task._utils.constants import API_KEY, DIAL_CHAT_COMPLETIONS_ENDPOINT
 from task._utils.model_client import DialModelClient
 from task._models.role import Role
 from task.image_to_text.openai.message import ContentedMessage, TxtContent, ImgContent, ImgUrl
-
 
 def start() -> None:
     project_root = Path(__file__).parent.parent.parent.parent
@@ -14,9 +19,17 @@ def start() -> None:
     with open(image_path, "rb") as image_file:
         image_bytes = image_file.read()
     base64_image = base64.b64encode(image_bytes).decode('utf-8')
-
-    # TODO:
+    
     #  1. Create DialModelClient
+    
+    client = DialModelClient(
+         api_key=API_KEY,
+         endpoint=DIAL_CHAT_COMPLETIONS_ENDPOINT,
+        deployment_name="gpt-4o"
+     )
+
+
+
     #  2. Call client to analise image:
     #    - try with base64 encoded format
     #    - try with URL: https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg
@@ -25,7 +38,24 @@ def start() -> None:
     #        Specification but since requests are going to the DIAL Core, we can use different models and DIAL Core
     #        will adapt them to format Gemini or Anthropic is using. In case if we go directly to
     #        the https://api.anthropic.com/v1/complete we need to follow Anthropic request Specification (the same for gemini)
-    raise NotImplementedError
+    
+    client.get_completion(
+        [
+            ContentedMessage(
+                role=Role.USER,
+                content=[
+                    TxtContent(
+                        text="What do you see on this picture?"
+                    ),
+                    ImgContent(
+                        image_url=ImgUrl(
+                            url=f"data:image/png;base64,{base64_image}"
+                        )
+                    )
+                ]
+            )
+        ]
+    )
 
-
+    
 start()
